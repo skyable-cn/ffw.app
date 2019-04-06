dee8302e35586eee93609383a542df9506015af5
mahonggang <1584157651@qq.com>
2019-04-06 21:18:29 +0800

    ss

--- src/main/java/com/ffw/app/config/CommonConfig.java
+++ src/main/java/com/ffw/app/config/CommonConfig.java
@@ -1,8 +1,11 @@
 package com.ffw.app.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+
+import com.ffw.app.filter.CommonFilter;
 
 @Configuration
 public class CommonConfig {
@@ -14,5 +17,16 @@ public class CommonConfig {
 		resourceBundleMessageSource.setBasename("i18n/messages");
 		return resourceBundleMessageSource;
 
+	}
+
+	@Bean
+	public FilterRegistrationBean testFilterRegistration() {
+
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(new CommonFilter());
+		registration.addUrlPatterns("/*");
+		registration.setName("commonFilter");
+		registration.setOrder(1);
+		return registration;
 	}
 }
---
+++ src/main/java/com/ffw/app/filter/CommonFilter.java
@@ -0,0 +1,44 @@
+package com.ffw.app.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommonFilter implements Filter {
+
+	private Logger logger = LoggerFactory.getLogger(CommonFilter.class);
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) arg0;
+		String openID = request.getParameter("openid");
+		if (StringUtils.isNotEmpty(openID)) {
+			logger.info("=========>> openid:" + openID);
+			request.getSession().setAttribute("openid", openID);
+		}
+
+		arg2.doFilter(arg0, arg1);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+}
