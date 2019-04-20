d274cb444f91b0f7f83dc437a9ab39e35462ad8a
mahonggang <1584157651@qq.com>
2019-04-20 16:51:51 +0800

    xx

---
+++ src/main/java/com/ffw/app/controller/MessageController.java
@@ -0,0 +1,54 @@
+package com.ffw.app.controller;
+
+import net.sf.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ffw.api.model.PageData;
+import com.ffw.app.config.WechatMiniConfig;
+import com.ffw.app.constant.IConstant;
+import com.ffw.app.util.HttpUtils;
+import com.ffw.app.util.JSSDKUtil;
+import com.ffw.app.util.RestTemplateUtil;
+
+@Controller
+public class MessageController extends BaseController {
+
+	@Autowired
+	WechatMiniConfig wechatMiniConfig;
+
+	@Autowired
+	RestTemplateUtil rest;
+
+	@RequestMapping(value = { "/message" })
+	@ResponseBody
+	public JSONObject message() {
+		logger.info("发送消息");
+		PageData pd = new PageData();
+		pd = this.getPageData();
+
+		pd = rest.post(IConstant.FFW_SERVICE_KEY, "groups/find", pd,
+				PageData.class);
+
+		String token = JSSDKUtil.AccessToken(wechatMiniConfig.getAppid(),
+				wechatMiniConfig.getAppsecret());
+
+		String json = "{\"touser\": \"" + pd.getString("openid")
+				+ "\",\"msgtype\": \"image\", \"image\": {\"media_id\": \""
+				+ pd.getString("MEDIA_ID") + "\"}}";
+		JSONObject rs = null;
+		try {
+			String str = HttpUtils.post(
+					"https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token="
+							+ token, json);
+			rs = JSONObject.fromObject(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.err.println(rs);
+		return rs;
+	}
+}
